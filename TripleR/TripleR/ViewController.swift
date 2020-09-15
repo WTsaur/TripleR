4b62495f90ffe8247afbe88321f7ba787c6904e3
William Tsaur <william.tsaur@gmail.com>
2020-09-14 19:49:13 -0700

    init xcode.proj commit

---
+++ TripleR/TripleR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TripleR
+//
+//  Created by William Tsaur on 9/14/20.
+//  Copyright © 2020 WORM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
