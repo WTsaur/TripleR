5a6cca98d96a2bc7aef6556a645f7aab569e4c48
William Tsaur <william.tsaur@gmail.com>
2020-09-15 17:00:41 -0700

    added button view switching functionality for settings, rights, report, and home buttons

---
+++ TripleR/TripleR/Controllers/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  TripleR
+//
+//  Created by William Tsaur on 9/15/20.
+//  Copyright © 2020 WORM. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recordButtonPressed(_ sender: UIButton) {
+        //open camera or start a voice recording
+    }
+    
+    @IBAction func settingsButtonPressed(_ sender: UIButton) {
+        //code
+    }
+    
+    @IBAction func rightsButtonPressed(_ sender: UIButton) {
+        //code
+    }
+    
+    @IBAction func reportButtonPressed(_ sender: UIButton) {
+        //code
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ TripleR/TripleR/Controllers/RightsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  RightsViewController.swift
+//  TripleR
+//
+//  Created by William Tsaur on 9/15/20.
+//  Copyright © 2020 WORM. All rights reserved.
+//
+
+import UIKit
+
+class RightsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func homeButtonPressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
