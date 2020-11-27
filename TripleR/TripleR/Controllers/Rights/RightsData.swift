95480f86892d2089cb4217ba8fc254bce9469cc8
Melissa Ma <mm16bn@my.fsu.edu>
2020-11-27 18:56:00 -0500

    Add Tips data to view controller

--- TripleR/TripleR/Controllers/Rights/RightsData.swift
+++ TripleR/TripleR/Controllers/Rights/RightsData.swift
@@ -66,32 +66,36 @@ public var rightsData: [Section] = [
 
 public var tipsData: [Section] = [
     Section(name: "Tips for Speaking with Law Enforcement", items: [
-                Item(name: "How do I identify myself to law enforcement?", detail: ""),
-                Item(name: "How do I find out why I'm being detained?", detail: "")
-    ]),
-    Section(name: "Can I know why I'm being detained?", items: [
-    ]),
-    Section(name: "Can I record police or take photos of them?", items: [
-    ]),
-    Section(name: "Should I show my personal identification to an officer?", items: [
+                Item(name: "Should I show my personal identification to an officer?", detail: "Yes, if you are stopped on suspicion of a crime or traffic violation. Note this does not apply to FBI or immigration agents."),
+                Item(name: "Can I ask if I'm free to leave?", detail: "Ask if you are free to leave. If the officer says \"yes\", exit the scene quietly and slowly. If you are not allowed to leave, you have the right to know why."),
+                Item(name: "How do I find out why I'm being detained?", detail: "You have the right to know why you are being arrested or detained. Ask the officer if you are free to go, and if not, ask why you are being stopped calmly.")
     ]),
     Section(name: "Tips for Dealing with ICE", items: [
-                Item(name: "ICE has no power without a signed warrant", detail: ""),
+        Item(name: "ICE has no power without a signed warrant", detail: ""),
         Item(name: "You have zero obligation to provide your name or “show your papers” to an ICE officer for any reason.", detail: "If you voluntarily provide immigration status, you can only do so if you have the papers on you.")
     ]),
     Section(name: "General Tips", items: [
-                Item(name: "Do not assume you can leave, ask first.", detail: ""),
-        Item(name: "If detained, tell the officer out loud you are exercising your right to remain silent.", detail: ""),
-        Item(name: "If you don’t have identification documents, you may choose to remain silent.", detail: "")
-        
+        Item(name: "Do not assume you can leave, ask first.", detail: ""),
+        Item(name: "If detained, tell the officer out loud you are exercising your right to remain silent.", detail: "Note that you have to identify yourself to police officers when you are stopped on suspicon of a crime or traffic violation."),
+        Item(name: "If you don’t have identification documents, you may choose to remain silent.", detail: "If you want to remain silent, tell the officer out loud you are using your right to remain silent."),
+        Item(name: "Do not interfere with police or obstruct them.", detail: "Do not argue, resist, or obstruct the officer, even if you are innocent and the police are violating your rights. Keep your hands where the officer can see them."),
+        Item(name: "You are not required to give permission for a search.", detail: "You must not resist physically, but you have the right to refuse a search. If you consent, it can be used against you in court. Say you out loud you refuse a search.")
+    ]),
+    Section(name: "What to Do in Certain Situations", items: [
+        Item(name: "If you are stopped in your vehicle:", detail: " • Turn on the interior light, open the window, and place both hands on the steering wheel in view of the officer.\n • If requested, show personal identification.\n • If the officer asks to search your vehicle, you can deny it vocally but not physically. However, they can search it anyway without your permission.\n • If you are a passenger, you can ask if you are free to leave. If yes, you can remain seated in silence or leave calmly.\n"),
+        Item(name: "If the police or immigration agents come to your residence:", detail: " • Do not let them in unless they have a signed warrant or court order.\n • Ask the officer to show you the signed document under the door or through a window.\n • If it is a search warrant, they can enter the home but only search the area for the items in the order.\n • If it is an arrest warrant, they can enter the home if they believe they are inside.\n • If it is a deportation/removal order, or  \"ICE warrant\", they cannot enter the home without your permission.\n • If you choose to speak with them outside your home, step out and close the door."),
+        Item(name: "If you are arrested:", detail: " • Do not resist arrest physically, even if you think it is wrong.\n • Say out loud you are using your right to remain silent and ask for a lawyer.\n • Do NOT say, sign, or make decision for anything without a lawyer.\n • You have the right to make one local call, and can ask for it. No officer may listen to your call.\n • Memorize the phone numbers of important contacts such as family and friends if you arrested.\n"),
+        Item(name: "If you are arrested or stopped while not a citizen:", detail: " • Do not talk about your immigration status with anyone besides your lawyer.\n • Ask your lawyer about the impact of the arrest on your immigration status.\n"),
+        Item(name: "If you are demonstrating or protesting around police presence:", detail: " • You may protest on public property, but on private property, its owners may set limits on your free speech. If you disobey their rules, they can order you off their property.\n • You can record the police anytime you are in an open public space, as long as you are not interfering with police activity.\n • If you are trying to record in a private place, where an incident is less visible tot he public, you can announce that you are recording what is going on as a precaution.\n")
     ])
+
 ]
 
-public var resourcesData: [Section] = [
-    Section(name: "ACLU", items: [
-            Item(name: "", detail: "")]),
-    Section(name: "Florida Statutes", items: [
-                Item(name: "", detail: ""
-                )])
-]
+//public var resourcesData: [Section] = [
+//    Section(name: "ACLU", items: [
+//            Item(name: "", detail: "")]),
+//    Section(name: "Florida Statutes", items: [
+//                Item(name: "", detail: ""
+//                )])
+//]
 
